0b31df26881bf47adf1cb3970ae3d19e5b388965
Vlad Sh <kausvirtual@gmail.com>
2023-09-09 02:23:46 +0300

    user skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PizzaModule } from './pizza/pizza.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PizzaModule],
+  imports: [PizzaModule, UserModule],
 })
 export class AppModule {}
