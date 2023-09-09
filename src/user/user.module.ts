b467cc80ca758070b2d45af8ebb287f04f22de94
Vlad Sh <kausvirtual@gmail.com>
2023-09-09 22:13:02 +0300

    imports fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
