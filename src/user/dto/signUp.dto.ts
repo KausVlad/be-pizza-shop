f871db5df77047573667a2b3497b42eac8d21bbc
Vlad Sh <kausvirtual@gmail.com>
2023-09-09 21:34:30 +0300

    added signUp.dto

---
+++ src/user/dto/signUp.dto.ts
@@ -0,0 +1,36 @@
+import { EnumSex } from '@prisma/client';
+import {
+  IsDate,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class signUpDto {
+  @IsString()
+  @IsNotEmpty()
+  userName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsPhoneNumber('UA')
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsEnum(EnumSex)
+  sex: EnumSex;
+
+  @IsDate()
+  birthDate: Date;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
