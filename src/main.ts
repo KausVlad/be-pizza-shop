8f6aa696122ea053e6cf2b3169b26345a7e66de4
Vlad Sh <kausvirtual@gmail.com>
2023-10-18 00:53:02 +0300

    setting cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN : false,
+
+      credentials: true,
+    },
+  });
   app.setGlobalPrefix('pizza-api');
   app.useGlobalPipes(
     new ValidationPipe({
