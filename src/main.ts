c6868164308d07d5c3cdb3cc8dacadbbd3629986
Vlad Sh <kausvirtual@gmail.com>
2023-11-21 03:20:50 +0200

    added new  cors ref

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN : false,
-
+      origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN_ADMIN] || false,
       credentials: true,
     },
   });
